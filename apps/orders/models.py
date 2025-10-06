4ea327879dc8b3e092941b585d99fb1c8bddebf8
tomas <tomastrigal680@gmail.com>
2025-10-06 19:16:15 -0300

    Models Cart and CartItem made

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.conf import settings
+from apps.products.models import Product
+
+class Cart(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE,
+        related_name='cart'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Carrito de {self.user.username}"
+
+    def total_items(self):
+        return sum(item.quantity for item in self.items.all())
+
+    def total_price(self):
+        return sum(item.quantity * item.product.price for item in self.items.all())
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey('products.Product')
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('cart', 'product')
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Reviews(models.Model):
+class Review(models.Model):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     img = models.ImageField(upload_to='product/reviews')
