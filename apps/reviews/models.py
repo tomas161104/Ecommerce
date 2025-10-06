103a995e431e24ea40c0af853d482e28e8d71f20
tomas <tomastrigal680@gmail.com>
2025-10-06 18:56:14 -0300

    Model reviw made

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Reviews(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='product/reviews')
+    content = models.CharField(max_length=255)
