819297a860602f4a5f745da43e416ed8ace2e6b6
tomas <tomastrigal680@gmail.com>
2025-10-06 19:28:36 -0300

    Models Order and OrderItem made

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Cart, CartItem
+from .models import Order, OrderItem
 
-# Register your models here.
-@admin.register(Cart)
-class CartAdmin (admin.ModelAdmin):
-    list_display = ['user', 'created_at']
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'status', 'total_price', 'payment', 'shipping_address', 'created_at']
 
 
-@admin.register(CartItem)
-class CartItemAdmin(admin.ModelAdmin):
-    list_display = ['cart', 'product', 'quantity', 'added_at']
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price']
--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,36 +1,56 @@
-from django.db import models
 
-# Create your models here.
 from django.db import models
 from django.conf import settings
+from apps.users.models import User
+from apps.payments.models import Payment
 from apps.products.models import Product
 
-class Cart(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.CASCADE,
-        related_name='cart'
+ORDER_STATUS_CHOICES = (
+    ('pending', 'Pending'),
+    ('paid', 'Paid'),
+    ('shipped', 'Shipped'),
+    ('completed', 'Completed'),
+    ('cancelled', 'Cancelled'),
+)
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='orders',
+        on_delete=models.CASCADE
     )
+    status = models.CharField(
+        max_length=10,
+        choices=ORDER_STATUS_CHOICES,
+        default='pending'
+    )
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    payment = models.OneToOneField(
+        Payment, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True,
+        related_name='order'
+    )
+    shipping_address = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Carrito de {self.user.username}"
-
-    def total_items(self):
-        return sum(item.quantity for item in self.items.all())
-
-    def total_price(self):
-        return sum(item.quantity * item.product.price for item in self.items.all())
+        return f"Order #{self.id} - {self.user.username}"
 
 
-class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey('products.Product')
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, 
+        related_name='items', 
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     quantity = models.PositiveIntegerField(default=1)
-    added_at = models.DateField(auto_now_add=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
-        unique_together = ('cart', 'product')
+        unique_together = ('order', 'product')
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
