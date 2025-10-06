51a3ad6c58a8d6dd9ff4cb7a9664e1b54968bc6d
tomas <tomastrigal680@gmail.com>
2025-10-06 18:31:44 -0300

    First commit: User already created

---
+++ apps/products/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from apps.users.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+
+
+
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    stock = models.IntegerField
+    img = models.ImageField(upload_to='product/img', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', related_name='products', on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
---
+++ ecommerce/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for ecommerce project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+SchemaView = get_schema_view(
+   openapi.Info(
+      title="ecommerce API",
+      default_version='v1',
+      description="ecommerce API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="tomastrigal680@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('docs/', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', SchemaView.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('apps.users.api.router'))
+]
